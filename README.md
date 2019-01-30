9a6681c1e4815b18b3b0ff1350726ddb1df6617c
Yauhen Artsiukhou <jsirex@gmail.com>
2019-01-30 14:00:49 +0300

    In the beginning there was darkness

---
+++ README.md
@@ -0,0 +1,3 @@
+# Overview
+
+This package provides `tomlcheck` binary.
---
+++ habitat/plan.sh
@@ -0,0 +1,33 @@
+pkg_name=tomlcheck
+pkg_origin=ya
+pkg_version="0.1.0.38"
+pkg_maintainer="Yauhen Artsiukhou <jsirex@gmail.com>"
+pkg_license=("Apache-2.0")
+pkg_source="https://github.com/vmchale/tomlcheck/releases/download/${pkg_version}/${pkg_name}-x86_64-unkown-linux-gnu"
+pkg_filename="${pkg_name}"
+pkg_shasum="118d26a04f8e463a21f53ddd7fd0394dadfe2164bb1c686d4f861d92862f2575"
+
+pkg_deps=(core/glibc core/gmp)
+pkg_build_deps=(core/patchelf)
+pkg_bin_dirs=(bin)
+
+do_unpack() {
+    pushd "${HAB_CACHE_SRC_PATH}" > /dev/null
+    chmod +x "$pkg_name"
+    popd
+}
+
+do_build() {
+    return 0
+}
+
+do_install() {
+    cp "${HAB_CACHE_SRC_PATH}/$pkg_name" "$pkg_prefix/bin/$pkg_name"
+    patchelf --interpreter "$(pkg_path_for core/glibc)/lib/ld-linux-x86-64.so.2" \
+             --set-rpath "$LD_RUN_PATH" "$pkg_prefix/bin/$pkg_name" \
+              "$pkg_prefix/bin/$pkg_name"
+}
+
+do_strip() {
+    return 0
+}
